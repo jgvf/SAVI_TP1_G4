3b5f42f7605b2c474307a19ffa955284fda050de
eramos1234 <eramos@ua.pt>
2023-10-29 16:26:36 +0000

    .

--- tentativa_recognition.py
+++ tentativa_recognition.py
@@ -28,18 +28,19 @@ class FaceRecognition:
     process_current_frame = True 
 
     def __init__(self):
-        pass
+        self.encode_faces()
+
         # encode faces
 
     
     def encode_faces(self):
-        for image in os.listdir('faces')
-        face_image = face_recognition.load_image_file(f'faces/{image}')
-        face_encoding = face_recognition.face_encodings(face_image)[0]
+        for image in os.listdir('faces'):
+            face_image = face_recognition.load_image_file(f'faces/{image}')
+            face_encoding = face_recognition.face_encodings(face_image)[0]
 
-        self.known_face_encodings.append(face_encoding)
-    
-    print(self.known_face_names)
+            self.known_face_encodings.append(face_encoding)
+            self.known_face_names.append(image)
+        print(self.known_face_names)
 
 if __name__ == '__main__':
     fr = FaceRecognition()
